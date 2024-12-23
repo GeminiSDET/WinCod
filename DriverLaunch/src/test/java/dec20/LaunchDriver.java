e071628b730e589bd054f2726cef7b999c9291a4
Azar <azarriju@gmail.com>
2024-12-23 23:23:59 +0530

    Add files via upload

---
+++ DriverLaunch/src/test/java/dec20/LaunchDriver.java
@@ -0,0 +1,15 @@
+package dec20;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchDriver {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://demo.opencart.com/");
+			
+
+	}
+
+}
